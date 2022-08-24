c9cc5762f5e44c90326792b6acdd1588fd09bf92
nanna-dk <nanna@e-nanna.dk>
2022-08-24 15:02:15 +0200

    Added controller

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,15 @@
 <?php
+
+/*
+ * This file is part of the package ku_phonebook.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 defined('TYPO3') or die('Access denied.');
+
+// Register ajax call
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+    'phonebookResults::phonebookResultsction',
+    'niversityOfCopenhagen\\KuPhonebook\\Controller\\phonebookResults->phonebookResultsction'
+);
